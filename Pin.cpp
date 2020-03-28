92b24111692296a47b18b914710ec087cb24e0b8
nikll123@hotmail.com <nikll123@hotmail.com>
2020-03-28 22:53:52 +0200

    Pin added
    Signed-off-by: nikll123@hotmail.com <nikll123@hotmail.com>

---
+++ Pin.cpp
@@ -0,0 +1,204 @@
+#include "Pin.h"
+
+// ------------------------------------
+Pin::Pin()
+{
+}
+
+// ------------------------------------
+Pin::Pin(const char *title, uint8_t pin, UnitType ut) : Unit(title, ut)
+{
+	_pin = pin;
+	_state = KS_NONE;
+}
+
+ShomCanBus Pin::CanBus = ShomCanBus();
+
+// ------------------------------------
+bool Pin::IsHigh()
+{
+	return ShomPinRead();
+}
+
+// ------------------------------------
+bool Pin::IsLow()
+{
+	return !IsHigh();
+}
+
+// ------------------------------------
+PinState Pin::GetState()
+{
+	return _state;
+}
+
+// ------------------------------------
+void Pin::SetState(PinState state, bool noLog)
+{
+	PinState2 ps2;
+	ps2.Old = _state;
+	bool val = (state == KS_ON);
+	if (_logicType == LT_INVERSE)
+		val = !val;
+	ShomPinWrite(val);
+	_state = state;
+	ps2.New = _state;
+	if (DoLogChanges)
+	{
+		if (ps2.Changed() && !noLog)
+			LogStates(ps2);
+	}
+}
+
+// ------------------------------------
+void Pin::LogStates(PinState2 ps2)
+{
+	LogInt_(_pin);
+	Log_("; ");
+	Log_(PinStateText(ps2.Old));
+	Log_(" -> ");
+	Log_(PinStateText(ps2.New));
+}
+
+// ------------------------------------
+/*String Pin::Title1()
+	{
+    return Unit::_title + "_" + String(_pin); 
+	}*/
+
+//------------------------------
+const char * Pin::StateText()
+{
+	return Pin::PinStateText(_state);
+}
+
+//------------------------------
+const char *Pin::PinStateText(PinState instate)
+{
+	switch (instate)
+	{
+	case KS_NONE:
+		return "KS_NONE";
+	case KS_ON:
+		return "KS_ON";
+	case KS_OFF:
+		return "KS_OFF";
+	default:
+		return "KS_unknown";
+	}
+}
+
+//------------------------------
+bool Pin::ShomPinRead()
+{
+	bool res = false;
+	CanBusState canbusres;
+	_state = KS_NONE;
+	byte pin = _pin;
+	if (pin < 100)
+	{
+		res = digitalRead(pin);
+		if (res)
+			_state = KS_ON;
+		else
+			_state = KS_OFF;
+	}
+	else
+	{
+		pin = _pin - 100;
+		unsigned int msgId = Pin::CanBus.SendCmd(CANBUS_READ, pin);
+		Pin::CanBus.ResponseDelay();
+		canbusres = Pin::CanBus.GetResponse(msgId, pin);
+		if (canbusres == CBS_LOW)
+			_state = KS_OFF;
+		else if (canbusres == CBS_HIGH)
+			_state = KS_ON;
+		else
+			_state = KS_ERR;
+		res = (canbusres == CBS_HIGH);
+	}
+
+	return res;
+}
+
+//------------------------------
+void Pin::ShomPinWrite(bool val)
+{
+	if (val == true)
+		_state = KS_ON;
+	else
+		_state = KS_OFF;
+
+	if (_pin < 100)
+	{
+		digitalWrite(_pin, val);
+	}
+	else
+	{
+		unsigned int id = Pin::CanBus.SendCmd(CANBUS_WRITE, _pin - 100, val);
+	}
+}
+
+//------------------------------
+void Pin::ShomPinMode(byte pinmode)
+{
+	_pinmode = pinmode;
+	if (_pin < 100)
+	{
+		pinMode(_pin, _pinmode);
+	}
+	else
+	{
+		unsigned int id = Pin::CanBus.SendCmd(CANBUS_MODE, _pin - 100, _pinmode);
+	}
+	//Log(PinModeText());
+}
+
+//------------------------------
+void Pin::LogInfo()
+{
+	Log_(_title);
+	Log_(": ");
+	Log(UnitTypeText());
+	Log_(", ");
+	Log(PinModeText());
+	Log_(", ");
+	Log(StateText());
+	Log_(", ");
+	LogInt(_pin);
+}
+
+//------------------------------
+const char *Pin::PinModeText()
+{
+	if (_pinmode == INPUT_PULLUP) // INPUT_PULLUP, INPUT, OUTPUT are Arduino's constants
+		return "INPUT_PULLUP";
+	else if (_pinmode == INPUT)
+		return "INPUT";
+	else if (_pinmode == OUTPUT)
+		return "OUTPUT";
+	else
+		return "Unknown";
+}
+
+//------------------------------
+String Pin::LogicTypeText()
+{
+	switch (_logicType)
+	{
+	case LT_NONE:
+		return "NONE";
+	case LT_NORMAL:
+		return "NORMAL";
+	case LT_INVERSE:
+		return "INVERSE";
+	default:
+		return "LogicTypeText: unknown-" + String(_logicType);
+	}
+}
+
+//------------------------------
+void Pin::SetLogicType(LogicType logicType)
+{
+	_logicType = logicType;
+}
---
+++ Pin.h
@@ -0,0 +1,90 @@
+/*
+Calss Pin - parent class for pins, Basic class for PinIn and PinOut classes
+Should not be used for object creation directly  
+*/
+
+#ifndef Pin_h
+	#define Pin_h
+	
+	#include "Unit.h"
+	#include "ShomCanBus.h"
+		
+   	//-------------------------------
+	enum PinState {KS_NONE	= 1, 
+					KS_ON	= 2, 
+					KS_OFF	= 3,
+					KS_ERR	= 500,
+					};	
+
+	//-------------------------------
+	struct PinState2 {
+					PinState 	Old;
+					PinState 	New;
+					bool High()	{return Old == KS_ON && New == KS_ON;};
+					bool Low()	{return Old == KS_OFF && New == KS_OFF;};
+					bool Front()	{return Old == KS_OFF && New == KS_ON;};
+					bool Back()		{return Old == KS_ON && New == KS_OFF;};
+					bool Changed()	{return Old != New;};
+					};	
+
+   	//-------------------------------
+	struct PinInfo {
+					String 		Title;
+					String 		UnitType;
+					String 		PinModeText;
+					String 		State;
+					uint8_t		Pin;
+					};	
+
+	//-------------------------------
+	struct PinRespCode{
+					int	RespCode = 0;
+					bool Value = false;
+					};	
+
+	//-------------------------------
+	enum LogicType {
+					LT_NONE,
+					LT_NORMAL, 
+					LT_INVERSE
+					};	
+
+   	//-------------------------------
+	class Pin : public Unit
+	{
+	public:
+	    					Pin();
+	    					Pin(const char * title, uint8_t pin, UnitType ut);
+	    
+		PinInfo 			GetInfo();
+		PinState			GetState();
+		void				SetState(PinState state, bool noLog);	
+		void				LogInfo();	
+		void 				LogStates(PinState2 ps2);
+		void				SetLogicType(LogicType logicType);
+
+		String 				LogicTypeText();
+		//String 				Title1();
+
+		const char *		PinStateText(PinState);
+		const char *		StateText();
+		const char * 		PinModeText();
+		bool				IsHigh();
+		bool				IsLow();
+		bool				ShomPinRead();
+		void 				ShomPinWrite(bool val);
+		void 				ShomPinMode(uint8_t pinmode);
+		static ShomCanBus 	CanBus;
+		bool				DoLogChanges = 1; 
+
+
+	protected:
+		uint8_t 			_pin;
+		PinState			_state = KS_NONE;
+		uint8_t 			_pinmode;
+		LogicType			_logicType; 
+
+	private:
+	};
+#endif
+
