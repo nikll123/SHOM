c61f37b881855c5097e84b62aeeec75641d3842d
nikll123@hotmail.com <nikll123@hotmail.com>
2020-03-31 09:34:56 +0300

    Conveyor added
    Signed-off-by: nikll123@hotmail.com <nikll123@hotmail.com>

---
+++ Conveyor.cpp
@@ -0,0 +1,260 @@
+#include "Conveyor.h"
+
+// =========   CONSTRUCTORS   =========
+Conveyor::Conveyor()
+{
+	_state = US_NOTINIT;
+}
+
+// ------------------------------------
+Conveyor::Conveyor(const char *title, uint8_t pinIn, uint8_t pinOut, uint8_t pinAuto, uint8_t pinLed) : Unit(title, UT_CONVEYOR)
+{
+	LedConveyor = Led(title, "_led", pinLed);
+	ContactorConveyor = Contactor(title, "_cont", pinIn, pinOut);
+	AtomatConveyor = PinIn(title, "_auto", pinAuto, LogicTypeAutomat);
+	_state = US_UNKNOWN;
+}
+
+// ------------------------------------
+void Conveyor::Init()
+{
+	LedConveyor.SetOn();
+	Log("Init");
+	_state = US_OFF;
+	ContactorConveyor.Init();
+	GetState();
+	delay(50); // to make LED light visible
+	LedConveyor.SetOff();
+}
+
+// ------------------------------------
+/*ConveyorInfo Conveyor::GetInfo()
+{
+	UnitInfo ui = Unit::GetInfo();
+	ContactorInfo ci = ContactorConveyor.GetInfo();
+	LedInfo li = LedConveyor.GetInfo();
+	PinInInfo ai = AtomatConveyor.GetInfo();
+	return {
+		ui.Title,
+		ui.UnitType,
+		GetConveyorStateText(_state),
+		ci.PinIn,
+		ci.PinOut,
+		ai.Pin,
+		li.Pin,
+		IsActive(),
+	};
+}*/
+
+// ------------------------------------
+void Conveyor::SetupLogic(LogicType ltIn, LogicType ltOut, LogicType ltAuto)
+{
+	Contactor::SetupLogic(ltIn, ltOut);
+	LogicTypeAutomat = ltAuto;
+}
+
+// ------------------------------------
+/*String Conveyor::GetInfoTxt()
+{
+	String str = "";
+	ConveyorInfo ci = GetInfo();
+	str = str + ci.Title + "; ";
+	str = str + ci.UnitType + "; ";
+	str = str + "in-" + String(ci.PinIn) + "; ";
+	str = str + "out-" + String(ci.PinOut) + "; ";
+	str = str + "auto-" + String(ci.PinAuto) + "; ";
+	str = str + "led-" + String(ci.PinLed) + "; ";
+	if (ci.Active)
+		str = str + "Active; ";
+	else
+		str = str + "Not active; ";
+
+	str = str + ci.State + ".";
+	return str;
+}*/
+
+//------------------------------
+const char *Conveyor::GetConveyorStateText(ConveyorState state)
+{
+	switch (state)
+	{
+	case US_NOTINIT:
+		return "NOTINIT";
+	case US_UNKNOWN:
+		return "UNKNOWN";
+	case US_ON:
+		return "ON";
+	case US_OFF:
+		return "OFF";
+	case US_STARTING:
+		return "STARTING";
+	case US_STOPPING:
+		return "STOPPING";
+	default:
+		return "ERROR";
+	}
+}
+
+// ------------------------------------
+/*ConveyorState2 Conveyor::GetState(String logText)
+{
+	//Log(logText);
+	return GetState();
+}*/
+
+// ------------------------------------
+ConveyorState2 Conveyor::GetState()
+{
+	ConveyorState2 us2 = {_state, _state};
+	if (_state != US_NOTINIT && _state < US_ERR)
+	{
+		ContactorState2 cs2 = ContactorConveyor.GetState();
+		PinState2 as2 = AtomatConveyor.GetState();
+		//bool err = false;
+		if (cs2.New >= CS_ERR)
+		{
+			SetErrState(US_ERR201);
+		}
+		else if (as2.New == KS_ON)
+		{
+			if (cs2.New == CS_OFF)
+				_state = US_OFF;
+			else if (cs2.New == CS_ON)
+				_state = US_ON;
+			else if (cs2.New == CS_STARTING)
+				_state = US_STARTING;
+			else if (cs2.New == CS_STOPPING)
+				_state = US_STOPPING;
+		}
+		else if (as2.New == KS_OFF)
+		{
+			if (cs2.New == CS_ON)
+				SetErrState(US_ERR202);
+			else if (cs2.New == CS_OFF)
+				SetErrState(US_ERR203);
+			else if (cs2.New == CS_STARTING)
+				SetErrState(US_ERR205);
+			else if (cs2.New == CS_STOPPING)
+				SetErrState(US_ERR206);
+			else
+				SetErrState(US_ERR207);
+		}
+		else
+		{
+			SetErrState(US_ERR208);
+		}
+
+		us2.New = _state;
+		IfChanged(us2);
+	}
+	LedConveyor.Refresh();
+	return us2;
+}
+
+// ------------------------------------
+void Conveyor::IfChanged(ConveyorState2 cs2)
+{
+	if (cs2.Old != cs2.New)
+	{
+		LogStates(cs2);
+		if (cs2.New == US_ON)
+			LedConveyor.SetOn();
+		else if (cs2.New == US_STARTING || cs2.New == US_STOPPING)
+			LedConveyor.SetBlink();
+		else if (cs2.New >= US_ERR)
+			LedConveyor.SetBlinkFast();
+		else
+			LedConveyor.SetOff();
+	}
+}
+
+// ------------------------------------
+void Conveyor::TurnOn()
+{
+	if (_state == US_OFF)
+	{
+		Log("TurnOn");
+		_Turn(US_STARTING);
+	}
+}
+
+// ------------------------------------
+void Conveyor::TurnOff()
+{
+	if (_state == US_ON || _state == US_STARTING)
+	{
+		Log("TurnOff");
+		_Turn(US_STOPPING);
+	}
+}
+
+// ------------------------------------
+void Conveyor::_Turn(ConveyorState csNew)
+{
+	if (csNew == US_STARTING || csNew == US_STOPPING)
+	{
+		ConveyorState2 cs2;
+		cs2.Old = _state;
+		//bool err = false;
+		if ((csNew == US_STARTING && cs2.Old == US_OFF)								   // start!
+			|| (csNew == US_STOPPING && (cs2.Old == US_ON || cs2.Old == US_STARTING))) // stop!
+		{
+			if (csNew == US_STARTING)
+				ContactorConveyor.TurnOn();
+			else
+				ContactorConveyor.TurnOff();
+			_state = csNew;
+		}
+		else
+		{
+			SetErrState(US_ERR209);
+		}
+
+		cs2.New = _state;
+		IfChanged(cs2);
+	}
+	else
+	{
+		Log_("_Turn: wrong arg ");
+		Log(GetConveyorStateText(csNew));
+	}
+}
+
+// ------------------------------------
+void Conveyor::Halt()
+{
+	ContactorConveyor.Halt();
+	LedConveyor.SetOff();
+	_state = US_HALT;
+}
+
+// ------------------------------------
+void Conveyor::SetErrState(UnitError err)
+{
+	LogErr(err);
+	ContactorConveyor.Halt();
+	_state = US_ERR;
+}
+
+// ------------------------------------
+void Conveyor::LogStates(ConveyorState2 cs2)
+{
+	Log_(GetConveyorStateText(cs2.Old));
+	Log_(" -> ");
+	Log(GetConveyorStateText(cs2.New));
+}
+
+// ------------------------------------
+void Conveyor::LogStatesPrevCurr(ConveyorStatePrevCurr cs2)
+{
+	Log_("Conveyor states: previous ");
+	Log_(GetConveyorStateText(cs2.Prev));
+	Log(", current ");
+	Log(GetConveyorStateText(cs2.Curr));
+}
+
+// ------------------------------------
+bool Conveyor::IsActive()
+{
+	return _state != US_NOTINIT;
+}
---
+++ Conveyor.h
@@ -0,0 +1,95 @@
+/*
+Conveyor class - represents a conveyor with contactor, automat and LED.
+*/
+
+#ifndef Conveyor_h
+	#define Conveyor_h
+	
+	#include "Unit.h"
+	#include "Led.h"
+	#include "PinIn.h"
+	#include "PinOut.h"
+	#include "Contactor.h"
+
+	enum ConveyorState{ 
+						US_NOTINIT		= 0,
+						US_UNKNOWN		= 1,
+						US_ON			= 2,
+						US_OFF			= 3,
+						US_STARTING		= 4,
+						US_STOPPING		= 5,
+						US_ERR			= 200,
+						US_HALT			= 299,
+					};
+						
+	struct ConveyorState2
+					{
+					ConveyorState Old;  
+					ConveyorState New;
+					
+					ConveyorState2(ConveyorState csOld = US_NOTINIT, ConveyorState csNew = US_NOTINIT):Old(csOld ),New(csNew ){};  
+					}; 
+
+	struct ConveyorStatePrevCurr
+					{
+					ConveyorState Prev;  
+					ConveyorState Curr;
+					
+					ConveyorStatePrevCurr(ConveyorState csPrev = US_NOTINIT, ConveyorState csCurr = US_NOTINIT):Prev(csPrev ),Curr(csCurr ){};  
+					}; 
+	
+    struct ConveyorInfo	
+					{
+					String   		Title;
+					String	 		UnitType;
+					String			State;
+					uint8_t			PinIn;
+					uint8_t     	PinOut;
+					uint8_t     	PinAuto;
+					uint8_t     	PinLed;
+					bool			Active;
+					unsigned int 	TimeOutOn;
+					}; 
+
+	static LogicType LogicTypeAutomat = LT_NORMAL;
+	
+	class Conveyor : public Unit
+	{
+	public:
+					    Conveyor();
+					    Conveyor(const char * title, uint8_t pinIn, uint8_t pinOut, uint8_t pinAuto, uint8_t pinLed);
+					    //Conveyor(const char * title, uint8_t pinIn, uint8_t pinOut, uint8_t pinAuto, uint8_t pinLed, unsigned long timeOutOn, unsigned long  timeOutOff);
+
+		ConveyorState2	GetState();
+		//ConveyorState2	GetState(String logText);
+		//ConveyorInfo 	GetInfo();
+		//String			GetInfoTxt();
+		static const char *	GetConveyorStateText(ConveyorState state);
+		void			Init();
+		void 			SetErrState(UnitError err);
+		void 			LogStates(ConveyorState2 cs2);
+		void 			LogStatesPrevCurr(ConveyorStatePrevCurr cs2);
+		void 			IfChanged(ConveyorState2 cs2);
+		static void		SetupLogic(LogicType ltIn, LogicType ltOut, LogicType ltAuto);
+		Led				LedConveyor;
+		Contactor		ContactorConveyor;
+		PinIn			AtomatConveyor;
+
+
+		void 			TurnOn();
+		void 			TurnOff();
+		void 			Halt();
+        bool			IsActive();
+
+	protected:
+		void LogKeysState();
+		
+	private:
+		ConveyorState2	_getStateAutomat();
+		ConveyorState2	_getStateContactor();
+		ConveyorState	_state;
+		ConveyorState	_stateprev;
+		void 			_Turn(ConveyorState csNew);
+	};
+#endif
+
