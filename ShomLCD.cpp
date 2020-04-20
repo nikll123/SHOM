bc8d1e43579369a284d88238dd1dc60a21aa10ec
nikll123@hotmail.com <nikll123@hotmail.com>
2020-04-20 23:46:28 +0300

    ShomLCD ++
    Signed-off-by: nikll123@hotmail.com <nikll123@hotmail.com>

--- ShomLCD.cpp
+++ ShomLCD.cpp
@@ -1,21 +1,51 @@
 #include "ShomLCD.h"
 
+LiquidCrystal_I2C ShomLCD::lcd1 = LiquidCrystal_I2C(addr, en, rw, rs, d4, d5, d6, d7, bl, blpol);
+
+
 // ------------------------------------
 ShomLCD::ShomLCD()
 {
 }
 
 // ------------------------------------
-ShomLCD::ShomLCD(const char *title, byte pin_sda, byte pin_slc) : Unit(title, UT_LCD)
+ShomLCD::ShomLCD(const char *title, byte pin_sda, byte pin_scl) : Unit(title, UT_LCD)
 {
-    Setup(title, pin_sda, pin_slc);
+    Setup(title, pin_sda, pin_scl);
 }
 
 
 // ------------------------------------
-void ShomLCD::Setup(const char *title, byte pin_sda, byte pin_slc)
+void ShomLCD::Setup(const char *title, byte pin_sda, byte pin_scl)
 {
-	_pin_sda = pin_sda;
-	_pin_slc = pin_slc;
-	_title = title;
+    _pin_sda = pin_sda;
+    _pin_scl = pin_scl;
+    _title = title;
+    // 
+}
+
+void ShomLCD::LogInfo()
+{
+    Log_(_title);
+    Log_("; addr=");
+    LogInt_(addr);
+    Log_("; en=");
+    LogInt_(en);
+    Log_("; rw=");
+    LogInt_(rw);
+    Log_("; rs=");
+    LogInt_(rs);
+    Log_("; d4=");
+    LogInt_(d4);
+    Log_("; d5=");
+    LogInt_(d5);
+    Log_("; d6=");
+    LogInt_(d6);
+    Log_("; d7=");
+    LogInt_(d7);
+    Log_("; bl=");
+    LogInt_(bl);
+    Log_("; blpol=");
+    LogInt(blpol);
+
 }
--- ShomLCD.h
+++ ShomLCD.h
@@ -3,22 +3,35 @@
 
 #include "Unit.h"
 
-#include <LiquidCrystal_I2C.h>
+#include "LiquidCrystal_I2C.h"
+
+// Set the pins on the I2C chip used for LCD connections:
+#define addr 0x27 // 0x3f
+#define en 2
+#define rw 1
+#define rs 0
+#define d4 4
+#define d5 5
+#define d6 6
+#define d7 7
+#define bl 3
+#define blpol POSITIVE
 
 //-------------------------------
 class ShomLCD : public Unit
 {
 public:
 	ShomLCD();
-	ShomLCD(const char *title, byte pin_sda, byte pin_slc);
+	ShomLCD(const char *title, byte pin_sda, byte pin_scl);
 	void Init();
-	void Setup(const char *title, byte pin_sda, byte pin_slc);
-	static LiquidCrystal_I2C lcd;
+	void Setup(const char *title, byte pin_sda, byte pin_scl);
+	void LogInfo();
+
+	static LiquidCrystal_I2C lcd1;
 
 private:
 	byte _pin_sda;
-	byte _pin_slc;
-
+	byte _pin_scl;
 };
 
 #endif
