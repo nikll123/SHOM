5730e6726ae6a94bdab18b67009cc796012c076f
nikll123@hotmail.com <nikll123@hotmail.com>
2020-04-21 23:31:46 +0300

    ShomLCD ++
    Signed-off-by: nikll123@hotmail.com <nikll123@hotmail.com>

--- ShomLCD.cpp
+++ ShomLCD.cpp
@@ -1,7 +1,6 @@
 #include "ShomLCD.h"
 
-LiquidCrystal_I2C ShomLCD::lcd1 = LiquidCrystal_I2C(addr, en, rw, rs, d4, d5, d6, d7, bl, blpol);
-
+LiquidCrystal_I2C ShomLCD::lcd = LiquidCrystal_I2C(ADDR, EN, RW, RS, D4, D5, D6, D7, BL, BLPOL);
 
 // ------------------------------------
 ShomLCD::ShomLCD()
@@ -14,38 +13,94 @@ ShomLCD::ShomLCD(const char *title, byte pin_sda, byte pin_scl) : Unit(title, UT
     Setup(title, pin_sda, pin_scl);
 }
 
-
 // ------------------------------------
 void ShomLCD::Setup(const char *title, byte pin_sda, byte pin_scl)
 {
     _pin_sda = pin_sda;
     _pin_scl = pin_scl;
     _title = title;
-    // 
+    lcd.begin(MAXCOL + 1, MAXROW + 1);
+    //
 }
 
+// ------------------------------------
+void ShomLCD::BackLightOn()
+{
+    lcd.backlight();
+}
+
+// ------------------------------------
+void ShomLCD::BackLightOff()
+{
+    lcd.noBacklight();
+}
+
+// ------------------------------------
+void ShomLCD::SetCursor(byte row, byte col)
+{
+    if (row > MAXROW || col > MAXCOL)
+        this->LogErrPosition(row, col);
+    else
+        lcd.setCursor(col, row);
+}
+
+// ------------------------------------
+void ShomLCD::Home()
+{
+    lcd.setCursor(0, 0);
+}
+
+// ------------------------------------
+void ShomLCD::Print(const char *str)
+{
+    lcd.print(str);
+}
+
+void ShomLCD::Print(byte row, byte col, const char *str)
+{
+    this->SetCursor(row, col);
+    this->Print(str);
+}
+
+// ------------------------------------
+void ShomLCD::Clear()
+{
+    lcd.clear();
+}
+
+// ------------------------------------
+void ShomLCD::LogErrPosition(byte row, byte col)
+{
+    LogIndent_();
+    Log_(_title);
+    Log_(" Error: Wrong position: row=");
+    LogInt_(row);
+    Log_("; col=");
+    LogInt(col);
+}
+
+// ------------------------------------
 void ShomLCD::LogInfo()
 {
     Log_(_title);
-    Log_("; addr=");
-    LogInt_(addr);
-    Log_("; en=");
-    LogInt_(en);
-    Log_("; rw=");
-    LogInt_(rw);
-    Log_("; rs=");
-    LogInt_(rs);
-    Log_("; d4=");
-    LogInt_(d4);
-    Log_("; d5=");
-    LogInt_(d5);
-    Log_("; d6=");
-    LogInt_(d6);
-    Log_("; d7=");
-    LogInt_(d7);
-    Log_("; bl=");
-    LogInt_(bl);
-    Log_("; blpol=");
-    LogInt(blpol);
-
+    Log_("; ADDR=");
+    LogInt_(ADDR);
+    Log_("; EN=");
+    LogInt_(EN);
+    Log_("; RW=");
+    LogInt_(RW);
+    Log_("; RS=");
+    LogInt_(RS);
+    Log_("; D4=");
+    LogInt_(D4);
+    Log_("; D5=");
+    LogInt_(D5);
+    Log_("; D6=");
+    LogInt_(D6);
+    Log_("; D7=");
+    LogInt_(D7);
+    Log_("; BL=");
+    LogInt_(BL);
+    Log_("; BLPOL=");
+    LogInt(BLPOL);
 }
--- ShomLCD.h
+++ ShomLCD.h
@@ -6,16 +6,18 @@
 #include "LiquidCrystal_I2C.h"
 
 // Set the pins on the I2C chip used for LCD connections:
-#define addr 0x27 // 0x3f
-#define en 2
-#define rw 1
-#define rs 0
-#define d4 4
-#define d5 5
-#define d6 6
-#define d7 7
-#define bl 3
-#define blpol POSITIVE
+#define ADDR 0x27 // 0x3f
+#define EN 2
+#define RW 1
+#define RS 0
+#define D4 4
+#define D5 5
+#define D6 6
+#define D7 7
+#define BL 3
+#define BLPOL POSITIVE
+#define MAXROW 1
+#define MAXCOL 15
 
 //-------------------------------
 class ShomLCD : public Unit
@@ -26,8 +28,16 @@ public:
 	void Init();
 	void Setup(const char *title, byte pin_sda, byte pin_scl);
 	void LogInfo();
+	void BackLightOn();
+	void BackLightOff();
+	void SetCursor(byte row, byte col);
+	void Home();
+	void Print(const char *str);
+	void Print(byte row, byte col, const char *str);
+	void Clear();
+	void LogErrPosition(byte row, byte col);
 
-	static LiquidCrystal_I2C lcd1;
+	static LiquidCrystal_I2C lcd;
 
 private:
 	byte _pin_sda;
